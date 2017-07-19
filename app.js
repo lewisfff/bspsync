54a7f07047829136ea19e831cb3769823c978e72
lewis <16711731@protonmail.com>
2017-07-19 09:32:17 +0100

    Scaffolding for what application needs to do

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 const watch = require('node-watch');
 
-console.log('watching for map changes');
+const mapDir = process.env.MAP_DIR;
 
-watch(process.env.MAP_DIR, { recursive: true, filter: /\.bsp$/ }, console.log);
+console.log('watching for map changes in', mapDir);
+
+watch(mapDir, { recursive: true, filter: /\.bsp$/ }, handleFile);
+
+function handleFile(event, path) {
+    if (event == 'update') {
+        console.log(path, 'has been updated!');
+        console.log('copy bsp to tmp');
+        console.log('pack files');
+        console.log('unique name');
+        console.log('bz2 zip');
+        console.log('upload to server');
+    }
+}
